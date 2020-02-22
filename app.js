e41513da8da9eb36229d91a6c98f0631e86caab2
ale182 <alestalves@gmail.com>
2020-02-22 19:06:43 -0300

    65. Refactoring do Multiroom Chat - Integrando o Socket.IO ao servidor NodeJS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var app = require('./config/server');
 
 // parametrizar porta de escuta
-app.listen(8090,function (){
+var server = app.listen(8090,function (){
     console.log('Servidor online');
-    });
+});
+
+require('socket.io').listen(server);

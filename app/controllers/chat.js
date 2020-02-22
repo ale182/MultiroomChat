062221e298f92d9ce45ba22eab4f78dc3fa1304a
ale182 <alestalves@gmail.com>
2020-02-22 18:56:05 -0300

    62. Recebendo e validando os dados do formulário de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function (application , req , res) {
+    var dadosForm = req.body ;
+
+    req.assert('apelido','Nome ou Apelido são obrigatórios').notEmpty();
+    req.assert('apelido','Nome ou Apelido devem conter entre 3 e 15 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+
+    if (erros){
+        res.render('index' , {validacao : erros});
+        return ;
+    }
+
     res.render("chat");
 };
